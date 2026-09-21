4e1f06e55506de42705f344975b77ef579c73c6c
Sakshi Shivaji Mane <sakshimane197@gmail.com>
2026-09-21 18:57:30 +0530

    Add files via upload

---
+++ Java_Basics/DataVar/one.java
@@ -0,0 +1,16 @@
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int age = 23;
+        String name = "sakshi ";
+        float salary = 100000.0f;
+        double height = 6.1;
+        char gender = 'F';
+        System.out.println("my name is " + name);
+        System.out.println("my age is " + age);
+        System.out.println("my salary is " + salary);
+        System.out.println("my height is " + height);
+        System.out.println("my gender is " + gender);
+    }
+}
---
+++ Java_Basics/InputOutput/one.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your age :");
+        int age = sc.nextInt();
+        System.out.println("your age is :" + age);
+
+    }
+}
