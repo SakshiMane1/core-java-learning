0beadcc3f79a33c865ea013471d41ff3bde188fa
Sakshi Shivaji Mane <sakshimane197@gmail.com>
2026-09-21 18:56:31 +0530

    Add files via upload

---
+++ ArraysAndStrings/Arrays/array.java
@@ -0,0 +1,17 @@
+package ArraysAndStrings.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50, 60, 70 };
+        int a[] = new int[5];
+        a[0] = 12;
+        a[1] = 15;
+        a[2] = 20;
+        a[3] = 4;
+        System.out.println(a[i]);
+ 
+        for (int i = 0; i<number.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
